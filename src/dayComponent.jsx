1374ded18e52b1c96d495c7a8f870d90eb8ffd11
Al Bungay <alerterb@gmail.com>
2019-05-23 00:26:30 -0700

    Calender working but reservations are wrong

--- src/dayComponent.jsx
+++ src/dayComponent.jsx
@@ -5,15 +5,37 @@ class DayComponent extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            availability : this.props.availability
         };
+
+        //bind functions
+        this.onFirstClickHandler = this.onFirstClickHandler.bind(this);
+        this.dayStateHandler = this.dayStateHandler.bind(this);
+    }
+
+    onFirstClickHandler(e) {
+        console.log(e);
+        this.props.dayClicked(this.props.date);
+    }
+
+    dayStateHandler() {
+        if (moment(this.props.date).isBefore(moment()) && this.props.exists) {
+            return "date_taken";
+        } else if (moment(this.props.date).isAfter(moment().add(90, 'd')) && this.props.exists) {
+            return "date_taken";
+        } else if (this.props.exists && this.props.dayAvailableToBook) {
+            return "date_available";
+        } else if (this.props.exists && !this.props.dayAvailableToBook) {
+            return "date_taken";
+        } else {
+            return "doesnt_exist";
+        }
     }
 
     render() {
         return (
-            <div className={this.props.exists}>
+            <div className={this.dayStateHandler()} onClick={this.onFirstClickHandler}>
             <p>
-                <font color='grey'>{this.props.typeOfDay}</font>
+                <font color='grey'>{this.props.dayNumber}</font>
             </p>
             </div>
         )
