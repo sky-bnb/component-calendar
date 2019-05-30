0075451148edbc5c239270211f6de618da891d8a
Al Bungay <alerterb@gmail.com>
2019-05-30 14:00:25 -0700

    Changed states CLI message for server status

--- database/index.js
+++ database/index.js
@@ -3,6 +3,6 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/calendar', { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error: '));
-db.once('open', () => console.log("STATUS: Mongoose DB Connected to Server"));
+db.once('open', () => console.log("MongoDB Connected to Server"));
 
 module.exports = { db };
